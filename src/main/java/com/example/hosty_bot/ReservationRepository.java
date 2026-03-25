7fbe56be442c0a7c7f1260ee8ea7beb16b599560
emilio1480 <emiliopulaj04@gmail.com>
2026-03-25 16:20:49 +0100

    feat: add room retrieval by code

--- src/main/java/com/example/hosty_bot/ReservationRepository.java
+++ src/main/java/com/example/hosty_bot/ReservationRepository.java
@@ -4,8 +4,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation,Long> {
     @Query("Select r.roomId from Reservation r where r.code = ?1 and r.name = ?2")
-    Integer getRoomIdByCodeAndName(String code, String name);
+    Optional<Integer> getRoomIdByCodeAndName(String code, String name);
+
+    @Query("Select r.roomId from Reservation r where r.code = ?1")
+    Optional<Integer> getRoomIdByCode(String code);
 }
